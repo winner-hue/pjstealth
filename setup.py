58cbaa8cb52737cde3daa280d0ec58c7a386c6c5
winner-hue <1344246287@qq.com>
2023-09-07 19:54:27 +0800

    init

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     description="浏览器特征抹除，及浏览器指纹随机化",
     long_description="",
     url="https://github.com/winner-hue/pjstealth",
-    packages=setuptools.find_packages(),
+    packages=['pjstealth'],
     python_require=">=3.9"
 )
